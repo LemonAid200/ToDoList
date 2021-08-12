c91168609a8dd360ce3874c5e3a937895f741ae2
RTU <54401330+LemonAid200@users.noreply.github.com>
2021-08-12 15:50:49 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,51 @@
+Vue.createApp({
+    data(){
+        return{
+            valueInput: '',
+            needDoList: [],
+            newOrDone: false
+
+        };
+    },
+    methods:{
+        handlyInput(event){
+            this.valueInput = event.target.value;
+        },
+        addTask(){
+             if(this.valueInput === '') {return};            
+            this.needDoList.push({
+                title: this.valueInput,
+                id: Math.random(),
+                status: false
+            });
+            this.valueInput = '';
+            
+        },
+
+        newPressed(){
+            this.newOrDone = true;
+            console.log(this.newOrDone)
+        },
+        
+        donePressed(){
+            this.newOrDone = false;
+            console.log(this.newOrDone)
+        },
+
+        doCheck(title, type){
+            index = this.needDoList.indexOf(title);
+            if(type === 'need'){
+                this.needDoList[index].status = true
+            }
+            else{
+                this.needDoList[index].status = false
+            }
+        }
+
+       
+
+
+
+
+    }
+}).mount('#app')
